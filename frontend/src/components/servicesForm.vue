405d64440195cefd81d2d0dc1e5a40dbe54dc88b
Ahmed Rahman <naimurrahmanloban@gmail.com>
2023-02-26 16:58:55 -0600

    psuedo script

---
+++ frontend/src/components/servicesForm.vue
@@ -0,0 +1,22 @@
+<script>
+import useVuelidate from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+import axios from 'axios'
+const apiURL = import.meta.env.VITE_ROOT_API
+
+export default{
+    data(){
+        return{
+            nameOfService: "",
+            descriptionOfService: "",
+            companyOfService: "",
+            statusOfService: false
+        };
+    },
+
+    methods: {
+        
+    }
+
+}
+</script>
